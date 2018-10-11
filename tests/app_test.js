253a5022c51969ea030737c1504ee6b4595c5fe3
angel2004 <angelvoyagera@gmail.com>
2018-10-11 14:12:47 +0300

    Update app_test.js

--- tests/app_test.js
+++ tests/app_test.js
@@ -19,13 +19,5 @@ describe('Basic routes tests', function() {
 
     })
 
-    it('GET to /pagecount should return 200', function(done){
-        chai.request(reqServer)
-        .get('/pagecount')
-        .end(function(err, res) {
-            res.should.have.status(200);
-            done();
-        })
-
-    })
+  
 })
